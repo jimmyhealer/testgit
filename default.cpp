b4b25b7a4b04d4809e45486794693047a3e0ac55
jimmyhealer <yahing6066@gmail.com>
2022-10-16 16:55:58 +0800

    First commit, initial git and upload defalut.cpp

---
+++ default.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define INF 0x3f3f3f3f
+#define LLINF 0x3f3f3f3f3f3f3f3f
+
+void sol(){
+
+}
+
+int main() {
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    sol();
+  }
+}
